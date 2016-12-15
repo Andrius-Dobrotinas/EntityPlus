dea851c37f2008f07816dc752c0b4ab5d5c026a3
Andrius-Dobrotinas <andrius.dobrotinas@gmail.com>
2016-12-15 21:34:36 +0200

    Added tests for EntityComparerByKeys

---
+++ EPCoreTests/EntityComparerByKeysTests.CompositeKey.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecordLabel.TheContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AndrewD.EntityPlus
+{
+    [TestClass]
+    public class EntityComparerByKeysTests_CompositeKey
+    {
+        private EntityComparerByKeys comparer;
+
+        [TestInitialize]
+        public void Init()
+        {
+            comparer = new EntityComparerByKeys();
+        }
+
+        [TestMethod]
+        public void EntitiesMustBeEqual_When_AllKeysAreEqual_DespiteOtherValues()
+        {
+            var keyProperties = GetKeyPropertiesMock();
+
+            var entity1 = new FakeClass { Id = 1, DoubleValue = 7.11, Value = "Hey Ho, Let's go!" };
+            var entity2 = new FakeClass { Id = 1, DoubleValue = 7.11, Value = "Seven Eleven" };
+
+            var result = comparer.CompareEntities(entity1, entity2, keyProperties);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void EntitiesMustBeNotEqual_When_OneOfTheKeysIsNotEqual_DespiteOtherValues()
+        {
+            var keyProperties = GetKeyPropertiesMock();
+
+            var entity1 = new FakeClass { Id = 0, DoubleValue = 7.11, Value = "Hey Ho, Let's go!" };
+            var entity2 = new FakeClass { Id = 1, DoubleValue = 7.11, Value = "Hey Ho, Let's go!" };
+            
+            var result = comparer.CompareEntities(entity1, entity2, keyProperties);
+
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void EntitiesMustBeNotEqual_When_OneOfTheKeysIsNotEqual_DespiteOtherValues_2()
+        {
+            var keyProperties = GetKeyPropertiesMock();
+
+            var entity1 = new FakeClass { Id = 1, DoubleValue = 7.11, Value = "Hey Ho, Let's go!" };
+            var entity2 = new FakeClass { Id = 1, DoubleValue = 0.5, Value = "Hey Ho, Let's go!" };
+
+            var result = comparer.CompareEntities(entity1, entity2, keyProperties);
+
+            Assert.AreEqual(false, result);
+        }
+
+
+        private List<IEntityKeyPropertyInfo> GetKeyPropertiesMock()
+        {
+            var keyMoq = new Moq.Mock<IEntityKeyPropertyInfo>();
+
+            var idProperty = typeof(FakeClass).GetProperty(nameof(FakeClass.Id));
+            keyMoq.Setup(x => x.PropertyInfo).Returns(idProperty);
+            keyMoq.Setup(x => x.IsForeignKey).Returns(false);
+
+            var key2Moq = new Moq.Mock<IEntityKeyPropertyInfo>();
+            var property2 = typeof(FakeClass).GetProperty(nameof(FakeClass.DoubleValue));
+            key2Moq.Setup(x => x.PropertyInfo).Returns(property2);
+            key2Moq.Setup(x => x.IsForeignKey).Returns(false);
+
+            var keyProperties = new List<IEntityKeyPropertyInfo>
+            {
+                keyMoq.Object, key2Moq.Object
+            };
+
+            return keyProperties;
+        }
+    }
+}
---
+++ EPCoreTests/EntityComparerByKeysTests.SingleKey.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecordLabel.TheContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AndrewD.EntityPlus
+{
+    [TestClass]
+    public class EntityComparerByKeysTests_SingleKey
+    {
+        private EntityComparerByKeys comparer;
+
+        [TestInitialize]
+        public void Init()
+        {
+            comparer = new EntityComparerByKeys();
+        }
+
+        [TestMethod]
+        public void EntitiesMustBeNotEqual_When_KeysNotEqual_DespiteOtherValues()
+        {
+            var keyProperties = GetKeyPropertiesMock();
+
+            var entity1 = new FakeClass { Id = 1, DoubleValue = 7.11, Value = "Seven Eleven" };
+            var entity2 = new FakeClass { Id = 2, DoubleValue = 7.11, Value = "Seven Eleven" };
+
+            var result = comparer.CompareEntities(entity1, entity2, keyProperties);
+
+            Assert.AreEqual(false, result, "Two entities with same values but different key property values are supposed to be not equal");
+        }
+
+        [TestMethod]
+        public void EntitiesMustBeNotEqual_When_KeysNotEqual_And_OneKeyIsDefault_DespiteOtherValues()
+        {
+            var keyProperties = GetKeyPropertiesMock();
+
+            var entity1 = new FakeClass { Id = 1, DoubleValue = 7.11, Value = "Seven Eleven" };
+            var entity2 = new FakeClass { Id = default(int), DoubleValue = 7.11, Value = "Seven Eleven" };
+
+            var result = comparer.CompareEntities(entity1, entity2, keyProperties);
+
+            Assert.AreEqual(false, result, "Two entities with same values but different key property values are supposed to be not equal");
+        }
+
+        [TestMethod]
+        public void EntitiesMustBeEqual_When_KeysAreEqual_But_OtherValuesAreNotEqual()
+        {
+            var keyProperties = GetKeyPropertiesMock();
+
+            var entity1 = new FakeClass { Id = 1, DoubleValue = 1.0, Value = "Hey Ho, Let's go!" };
+            var entity2 = new FakeClass { Id = 1, DoubleValue = 7.11, Value = "Seven Eleven" };
+
+            var result = comparer.CompareEntities(entity1, entity2, keyProperties);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void EntitiesMustBeEqual_When_KeysAreEqual_And_BothAreDefault()
+        {
+            var keyProperties = GetKeyPropertiesMock();
+
+            var entity1 = new FakeClass { Id = default(int), DoubleValue = 1.0, Value = "What's up, dog?" };
+            var entity2 = new FakeClass { Id = default(int), DoubleValue = 7.11, Value = "Seven Eleven" };
+
+            var result = comparer.CompareEntities(entity1, entity2, keyProperties);
+
+            Assert.AreEqual(true, result);
+        }
+
+        private List<IEntityKeyPropertyInfo> GetKeyPropertiesMock()
+        {
+            var keyMoq = new Moq.Mock<IEntityKeyPropertyInfo>();
+
+            var idProperty = typeof(FakeClass).GetProperty(nameof(FakeClass.Id));
+            keyMoq.Setup(x => x.PropertyInfo).Returns(idProperty);
+            keyMoq.Setup(x => x.IsForeignKey).Returns(false);
+
+            var keyProperties = new List<IEntityKeyPropertyInfo>
+            {
+                keyMoq.Object
+            };
+
+            return keyProperties;
+        }
+    }
+
+    internal class FakeClass
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public double DoubleValue { get; set; }
+    }
+}
